82f0bfd432f27e3941d0c78e235402c8432ff0ca
Alin Babu <alinbabu2010@gmail.com>
2020-09-27 16:26:27 +0530

    Media file setting added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,4 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
