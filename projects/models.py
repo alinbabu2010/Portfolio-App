4ab70bb786f2cb9ce8f24524318f3feae5e1c8a5
Alin Babu <alinbabu2010@gmail.com>
2020-09-27 16:21:50 +0530

    Changed the upload folder

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Projects(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images")
     summary = models.TextField(max_length=500)
 
     def __str__(self):
