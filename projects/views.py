31423deb81c4d4f9d6157655cfb877630a3620fe
Alin Babu <alinbabu2010@gmail.com>
2020-09-27 19:22:53 +0530

    Project details page and its view implemented

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Projects
 
 def index(request):
     projects = Projects.objects
-    return render(request,'projects/index.html',{'projects' : projects})
+    return render(request, 'projects/index.html', {'projects': projects})
+    
+def detail(request, project_id):
+    project_detail = get_object_or_404(Projects,pk=project_id)
+    return render(request,'projects/details.html',{'project' : project_detail })
